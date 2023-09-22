f4e81a76b557a8edce77f1f6e1495b78b6eb83cb
sahsa246810 <142878525+sahsa246810@users.noreply.github.com>
2023-09-22 16:03:59 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
+import time
 from selenium.webdriver.common.by import By
 def test_items(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
+    time.sleep(3)
     basket = None
     basket = browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
     assert basket is not None, 'element not found'
